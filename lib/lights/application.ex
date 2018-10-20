c3ed151b3ea1081495a03ce234e22220f3888d41
Michael Ries <michael@riesd.com>
2018-10-19 20:39:41 -0600

    supervise neopixel driver

--- lib/lights/application.ex
+++ lib/lights/application.ex
@@ -8,24 +8,10 @@ defmodule Lights.Application do
   use Application
 
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Lights.Supervisor]
-    Supervisor.start_link(children(@target), opts)
-  end
-
-  # List all child processes to be supervised
-  def children("host") do
-    [
-      # Starts a worker by calling: Lights.Worker.start_link(arg)
-      # {Lights.Worker, arg},
+    children = [
+      {Nerves.Neopixel, [pin: 18, count: 60]},
     ]
-  end
-
-  def children(_target) do
-    [
-      # Starts a worker by calling: Lights.Worker.start_link(arg)
-      # {Lights.Worker, arg},
-    ]
+    Supervisor.start_link(children, opts)
   end
 end
