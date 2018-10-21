7fd9fa315352329731f9ddc8faff29c1a27221be
Michael Ries <michael@riesd.com>
2018-10-20 21:20:44 -0600

    tests for Bounce animation functions

--- lib/lights/bounce.ex
+++ lib/lights/bounce.ex
@@ -1,45 +1,42 @@
 defmodule Lights.Bounce do
   defstruct which_pixel: 0,
-            direction:   :up,
-            intensity:   31,
-            render_pause: 33
-
-  def change_direction(%__MODULE__{direction: :up}=state) do
-    %{ state | direction: :down }
-  end
-  def change_direction(%__MODULE__{direction: :down}=state) do
-    %{ state | direction: :up }
-  end
-
-  def next(%__MODULE__{which_pixel: 59, direction: :up}=state) do
-    %{ state | which_pixel: 58, direction: :down }
-  end
-  def next(%__MODULE__{which_pixel: p, direction: :up}=state) do
-    %{ state | which_pixel: p + 1 }
-  end
-  def next(%__MODULE__{which_pixel: 0, direction: :down}=state) do
-    %{ state | which_pixel: 1, direction: :up }
-  end
-  def next(%__MODULE__{which_pixel: p, direction: :down}=state) do
-    %{ state | which_pixel: p - 1 }
-  end
+            direction:   :up
 
   def pixels(%__MODULE__{which_pixel: which}) do
     {0, 0, 0}
     |> List.duplicate(60)
     |> List.replace_at(which, {255, 255, 255})
   end
-end
 
-defimpl Lights.Animation, for: Lights.Bounce do
-  alias Lights.Bounce
+  defimpl Lights.Animation do
+    alias Lights.Bounce
 
-  def next(%Bounce{}=bounce), do: Bounce.next(bounce)
-  def render(%Bounce{}=bounce) do
-    %{
-      pixels:    Bounce.pixels(bounce),
-      intensity: 15,
-      pause:     30,
-    }
+    def next(%Bounce{which_pixel: 59, direction: :up}=state) do
+      %{ state | which_pixel: 58, direction: :down }
+    end
+    def next(%Bounce{which_pixel: p, direction: :up}=state) do
+      %{ state | which_pixel: p + 1 }
+    end
+    def next(%Bounce{which_pixel: 0, direction: :down}=state) do
+      %{ state | which_pixel: 1, direction: :up }
+    end
+    def next(%Bounce{which_pixel: p, direction: :down}=state) do
+      %{ state | which_pixel: p - 1 }
+    end
+
+    def render(%Bounce{}=bounce) do
+      %{
+        pixels:    Bounce.pixels(bounce),
+        intensity: 15,
+        pause:     30,
+      }
+    end
+
+    def change_direction(%Bounce{direction: :up}=state) do
+      %{ state | direction: :down }
+    end
+    def change_direction(%Bounce{direction: :down}=state) do
+      %{ state | direction: :up }
+    end
   end
 end
--- test/bounce_test.exs
+++ test/bounce_test.exs
@@ -1,9 +1,42 @@
 defmodule Lights.BounceTest do
   use ExUnit.Case, async: true
-  alias Lights.Bounce
+  alias Lights.{Animation,Bounce}
 
   test "it starts out on the first pixel" do
     bounce = %Bounce{}
-    assert bounce.which_pixel == 0
+    assert bounce == %Bounce{which_pixel: 0, direction: :up}
+  end
+
+  test "it moves up" do
+    bounce = %Bounce{} |> Animation.next()
+    assert bounce == %Bounce{which_pixel: 1, direction: :up}
+  end
+
+  test "it bounces off the top end of the strip" do
+    bounce = %Bounce{which_pixel: 59, direction: :up} |> Animation.next()
+    assert bounce == %Bounce{which_pixel: 58, direction: :down}
+  end
+
+  test "it moves down" do
+    bounce = %Bounce{which_pixel: 50, direction: :down} |> Animation.next()
+    assert bounce == %Bounce{which_pixel: 49, direction: :down}
+  end
+
+  test "it bounces off the bottom end of the strip" do
+    bounce = %Bounce{which_pixel: 0, direction: :down} |> Animation.next()
+    assert bounce == %Bounce{which_pixel: 1, direction: :up}
+  end
+
+  test "it renders the pixels" do
+    %{
+      pixels:    [first | rest],
+      pause:     pause,
+      intensity: intensity,
+    } = Animation.render(%Bounce{})
+    assert pause == 30
+    assert intensity == 15
+    assert Enum.count(rest) == 59
+    assert first == {255, 255, 255}
+    assert Enum.all?(rest, &(&1 == {0, 0, 0}))
   end
 end
