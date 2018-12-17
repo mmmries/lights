0f3417886c92476189622ff890a1244eba4a0797
Michael Ries <michael@riesd.com>
2018-12-17 07:05:43 -0700

    Wire up Painter to use the Blinkchain API

--- lib/lights/application.ex
+++ lib/lights/application.ex
@@ -16,9 +16,8 @@ defmodule Lights.Application do
   else
     defp children() do
       [
-        #{Nerves.Neopixel, [pin: 18, count: 8 * 32]},
-        #{Lights.Painter, nil},
-        #{Lights.Buttons, nil},
+        {Lights.Painter, nil},
+        {Lights.Buttons, nil},
       ]
     end
   end
